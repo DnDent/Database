94460e8b04080499cd2fe7438504c4ba8df9eca1
DnDent <v.dentener@gmail.com>
2026-01-09 12:23:00 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,17 +1,5 @@
 /* global CustomFunctions */
 CustomFunctions.associate("DATA", function (identifier, date) {
-  try {
-    var xhr = new XMLHttpRequest();
-    // Try to call a simple public API (httpbin.org returns whatever you send)
-    xhr.open("GET", "https://httpbin.org/get", false); // false = synchronous
-    xhr.send();
-    
-    if (xhr.status === 200) {
-      return [["HTTP request worked! Status: " + xhr.status]];
-    } else {
-      return [["HTTP failed with status: " + xhr.status]];
-    }
-  } catch (e) {
-    return [["ERROR: " + e.message]];
-  }
+  var url = "https://vinhuys.sharepoint.com/sites/VinHuys/_api/web/lists/getbytitle('Vinhuys database')/items";
+  return [["URL ready: " + url.substring(0, 50) + "..."]];
 });
