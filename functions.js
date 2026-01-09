87669c663c38acea57d07c988913a178a26f5375
DnDent <v.dentener@gmail.com>
2026-01-09 12:34:15 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,5 +1,12 @@
 /* global CustomFunctions */
 CustomFunctions.associate("DATA", function (identifier, date) {
-  var url = "https://vinhuys.sharepoint.com/sites/VinHuys/_api/web/lists/getbytitle('Vinhuys database')/items";
-  return [["URL ready: " + url.substring(0, 50) + "..."]];
+  try {
+    var xhr = new XMLHttpRequest();
+    var url = "https://vinhuys.sharepoint.com/sites/VinHuys/_api/web/lists/getbytitle('Vinhuys database')/items";
+    xhr.open("GET", url, false); // false = synchronous
+    
+    return [["XHR created and opened"]];
+  } catch (e) {
+    return [["ERROR: " + e.message]];
+  }
 });
