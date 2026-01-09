e498a74987d64cc6159f522b8f6c2a014b49d52b
DnDent <v.dentener@gmail.com>
2026-01-09 03:31:21 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,4 +1,10 @@
-/* global CustomFunctions */
+/* global CustomFunctions, msal */
 CustomFunctions.associate("DATA", function (identifier, date) {
-  return [["WORKS!"]];
+  try {
+    var hasMsal = (typeof msal !== "undefined") && msal && msal.PublicClientApplication;
+    var msg = hasMsal ? "MSAL_OK" : "MSAL_MISSING";
+    return [[msg]];
+  } catch (e) {
+    return [["ERR: " + (e && e.message ? e.message : String(e))]];
+  }
 });
