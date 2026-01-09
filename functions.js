b483dedccfdd7fb9a55d92fb64e63557be65eb06
DnDent <v.dentener@gmail.com>
2026-01-09 03:54:46 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,14 +1,4 @@
-/* global CustomFunctions, msal */
-
+/* global CustomFunctions */
 CustomFunctions.associate("DATA", function (identifier, date) {
-  try {
-    // Check if MSAL is available
-    if (typeof msal === "undefined") {
-      return [["ERROR: MSAL library not loaded"]];
-    }
-    
-    return [["MSAL library loaded OK"]];
-  } catch (e) {
-    return [["ERROR: " + (e.message || String(e))]];
-  }
+  return [["WORKS!"]];
 });
