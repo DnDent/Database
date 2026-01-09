aab40d97e70ce6f7acb4958fec296c393a979c02
DnDent <v.dentener@gmail.com>
2026-01-09 04:21:41 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,6 +1,26 @@
 /* global CustomFunctions, OfficeRuntime */
+
+async function getAccessToken() {
+  try {
+    // Use Office's built-in SSO to get a token
+    var token = await OfficeRuntime.auth.getAccessToken({
+      allowSignInPrompt: true,
+      allowConsentPrompt: true,
+      forMSGraphAccess: true
+    });
+    return token;
+  } catch (error) {
+    throw new Error("Auth failed: " + error.code);
+  }
+}
+
 CustomFunctions.associate("DATA", function (identifier, date) {
-  // Use Office's built-in auth instead of MSAL
-  // This is simpler and designed specifically for Office Add-ins
-  return [["Using Office auth instead"]];
+  return new CustomFunctions.CancelablePromise(async (resolve, reject) => {
+    try {
+      var token = await getAccessToken();
+      resolve([["Token: " + token.substring(0, 20) + "..."]]);
+    } catch (error) {
+      resolve([["ERROR: " + error.message]]);
+    }
+  });
 });
