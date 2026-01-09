2e9258d575f3fd0365ce0ac3157400559ace5c3a
DnDent <v.dentener@gmail.com>
2026-01-09 18:54:08 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -20,18 +20,42 @@ async function loadData() {
     return data;
 }
 
+function lookupValue(data, identifier, date) {
+    for (var i = 0; i < data.length; i++) {
+        if (data[i].Identifier === identifier && data[i].Date === date) {
+            return data[i].Value;
+        }
+    }
+    return "Not found";
+}
+
 async function getData(identifier, date) {
     try {
         var data = await loadData();
         
-        for (var i = 0; i < data.length; i++) {
-            if (data[i].Identifier === identifier && data[i].Date === date) {
-                return data[i].Value;
+        // With dimensionality: "matrix", Excel ALWAYS passes 2D arrays
+        // Single cell "TEST001" comes as [["TEST001"]]
+        // Range A1:A3 comes as [["val1"], ["val2"], ["val3"]]
+        
+        var numRows = identifier.length;
+        var numCols = identifier[0].length;
+        
+        // Build result matrix
+        var result = [];
+        for (var row = 0; row < numRows; row++) {
+            var resultRow = [];
+            for (var col = 0; col < numCols; col++) {
+                var id = identifier[row][col];
+                var dt = date[row][col];
+                resultRow.push(lookupValue(data, id, dt));
             }
+            result.push(resultRow);
         }
-        return "Not found";
+        
+        return result;
+        
     } catch (error) {
-        return "Error: " + error.message;
+        return [["Error: " + error.message]];
     }
 }
 
