2e081737fb2b03c57b9d2685768502c45aa55930
DnDent <v.dentener@gmail.com>
2026-01-09 12:41:21 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -3,11 +3,9 @@ CustomFunctions.associate("DATA", function (identifier, date) {
   try {
     var xhr = new XMLHttpRequest();
     var url = "https://vinhuys.sharepoint.com/sites/VinHuys/_api/web/lists/getbytitle('Vinhuys database')/items";
-    xhr.open("GET", url, false);
-    xhr.setRequestHeader("Accept", "application/json;odata=verbose");
-    xhr.send();
+    xhr.open("GET", url, false); // false = synchronous
     
-    return [["Status: " + xhr.status]];
+    return [["XHR created and opened"]];
   } catch (e) {
     return [["ERROR: " + e.message]];
   }
