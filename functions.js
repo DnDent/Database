0e9260935f3dd65f224f515e32f8c67c1cfc7d59
DnDent <v.dentener@gmail.com>
2026-01-09 03:19:18 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,11 +1,4 @@
-/* global CustomFunctions, msal */
+/* global CustomFunctions */
 CustomFunctions.associate("DATA", function (identifier, date) {
-  try {
-    var hasMsal = (typeof msal !== "undefined") && msal && msal.PublicClientApplication;
-    var msg = hasMsal ? "MSAL_OK" : "MSAL_MISSING";
-    // Must return a matrix because your JSON says dimensionality: "matrix"
-    return [[msg]];
-  } catch (e) {
-    return [["ERR: " + (e && e.message ? e.message : String(e))]];
-  }
+  return [["WORKS"]];
 });
