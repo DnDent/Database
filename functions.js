bf6092cbb10b9a9d91040ba3dc45db7fef4d0e1d
DnDent <v.dentener@gmail.com>
2026-01-09 11:45:43 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,13 +1,4 @@
 /* global CustomFunctions */
-
-// SharePoint site and list details
-var siteUrl = "https://vinhuys.sharepoint.com/sites/VinHuys";
-var listName = "Vinhuys database";
-
 CustomFunctions.associate("DATA", function (identifier, date) {
-  // Build REST API URL to get list items
-  var apiUrl = siteUrl + "/_api/web/lists/getbytitle('" + listName + "')/items";
-  
-  // For now, just return the URL we would call
-  return [["Will call: " + apiUrl]];
+  return [["WORKS!"]];
 });
