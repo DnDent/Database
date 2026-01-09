f0fb61f73c045489d09b0be7632eedd4769dc6ed
DnDent <v.dentener@gmail.com>
2026-01-09 17:26:03 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,28 +1,56 @@
-var DATA_CACHE = null;
-var BLOB_URL = "https://vinhuysstorage.blob.core.windows.net/data/vinhuys.json?sp=r&st=2026-01-09T14:30:06Z&se=2027-01-09T22:45:06Z&spr=https&sv=2024-11-04&sr=b&sig=2j6d753ENDCFlkHpxZQcifIri54p2TEWYGS2jZjHTk8%3D";
+/* global CustomFunctions, OfficeRuntime */
 
-function getData(identifier, date) {
-  return fetch(BLOB_URL)
-    .then(function(response) { 
-      if (!response.ok) {
-        return "Fetch error: " + response.status;
-      }
-      return response.json(); 
-    })
-    .then(function(data) {
-      if (typeof data === "string") {
-        return data;
-      }
-      for (var i = 0; i < data.length; i++) {
-        if (data[i].Identifier === identifier && data[i].Date === date) {
-          return data[i].Value;
+var DATA_CACHE = null;
+var FUNCTION_URL = "https://vinhuys-function.azurewebsites.net/api/getData";
+
+async function getAccessToken() {
+    try {
+        var token = await OfficeRuntime.auth.getAccessToken({
+            allowSignInPrompt: true,
+            allowConsentPrompt: true
+        });
+        return token;
+    } catch (error) {
+        throw new Error("Auth failed: " + error.message);
+    }
+}
+
+async function loadData() {
+    if (DATA_CACHE) {
+        return DATA_CACHE;
+    }
+
+    var token = await getAccessToken();
+    
+    var response = await fetch(FUNCTION_URL, {
+        headers: {
+            'Authorization': 'Bearer ' + token
         }
-      }
-      return "Not found";
-    })
-    .catch(function(error) {
-      return "Error: " + error.message;
     });
+
+    if (!response.ok) {
+        var errorText = await response.text();
+        throw new Error("API error: " + response.status + " " + errorText);
+    }
+
+    var data = await response.json();
+    DATA_CACHE = data;
+    return data;
+}
+
+async function getData(identifier, date) {
+    try {
+        var data = await loadData();
+        
+        for (var i = 0; i < data.length; i++) {
+            if (data[i].Identifier === identifier && data[i].Date === date) {
+                return data[i].Value;
+            }
+        }
+        return "Not found";
+    } catch (error) {
+        return "Error: " + error.message;
+    }
 }
 
 CustomFunctions.associate("DATA", getData);
